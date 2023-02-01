49e44d44402ca53b47a22905ba24b6dba9fdfbba
Yong Park <pyongho0306@gmail.com>
2023-01-31 21:05:26 -0800

    Add simple home content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div className="container">
+      <img
+        src="https://media.licdn.com/dms/image/C5603AQGpjoopTdN3lQ/profile-displayphoto-shrink_800_800/0/1612428126745?e=1680739200&v=beta&t=ibltgJVe03quCf0bIwo28ny9ZNpPb5qbjDYJX97xwmg"
+        alt=""
+      />
+      <h1>Hello, I'm Yong Park!</h1>
+      <h3>Software Engineer</h3>
     </div>
   );
 };
