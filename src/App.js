5a742ce52d29c51cce24fd322970a25cb3c9204f
Yong Park <pyongho0306@gmail.com>
2023-01-31 20:50:30 -0800

    Create basic pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact/Contact";
+import Resume from "./pages/Resume/Resume";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="resume" element={<Resume />} />
+        <Route path="projects" element={<Projects />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Resume/Resume.jsx
@@ -0,0 +1,9 @@
+const Resume = () => {
+  return (
+    <div>
+      <h1>RESUME</h1>
+    </div>
+  );
+};
+
+export default Resume;
