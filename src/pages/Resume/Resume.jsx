c99274accff440e06faffee215184147d2d286ee
Yong Park <pyongho0306@gmail.com>
2023-02-01 18:42:44 -0800

    Add content for resume page

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,7 +1,11 @@
+import "./Resume.css";
+
 const Resume = () => {
   return (
-    <div>
+    <div className="resumeContainer">
       <h1>RESUME</h1>
+      <img src="./resume.png" alt="" />
+      <h4>Click on the icon to download resume</h4>
     </div>
   );
 };
